73c3a20e73ee8aa77b470b0e627b803107d233f7
pikmr <benjahergar@gmail.com>
2022-10-01 20:28:20 +0200

    Aplica DomainModel + Pattern repository + Vertical Slice

---
+++ ServiMotor.Api/Controllers/OilsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiMotor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OilsController : ControllerBase
+    {
+        private readonly ILogger<OilsController> _logger;
+
+        public OilsController(ILogger<OilsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            // TODO
+            return null;
+        }
+    }
+}
